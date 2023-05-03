d47c6d8ea9b8258714529cea4f88d5347a07941d
aless124 <alessfara28@gmail.com>
2023-05-03 09:41:05 +0200

    Fix DataAPI

--- Final_Exam/DataAPI.py
+++ Final_Exam/DataAPI.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-<<<<<<< Updated upstream
-=======
+
+
 from dotenv import load_dotenv
 import os
 
@@ -12,14 +12,13 @@ PASSWORD = os.getenv('PASSWORD')
 
 print(USERNAME)
 print(PASSWORD)
->>>>>>> Stashed changes
+
 
 app = Flask(__name__)  # Pour initialiser l'application
 app.config["DEBUG"] = True # Pour activer le débogage et le rechargement automatique du code
 
 
 app.config['MONGO_DBNAME'] = 'final_exam'
-<<<<<<< Updated upstream
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/final_exam'
 mongo = PyMongo(app)
 collection = mongo.db.final_exam
@@ -29,16 +28,12 @@ def FixTitle():
     for doc in collection.find():
         titre = str(doc['Titre']).replace('\n', '').replace('\r', '').strip()
         collection.update_one({'_id': doc['_id']}, {'$set': {'Titre': titre}})
-=======
+
 #app.config['MONGO_URI'] = 'mongodb://localhost:27017/final_exam'
 app.config['MONGO_URI'] = 'mongodb+srv://'+USERNAME+':'+PASSWORD+'@cluster0.tzf6zx8.mongodb.net/final_exam'
 mongo = PyMongo(app)
 collection = mongo.db.final_exam
 
-for doc in collection.find():
-    titre = str(doc['Titre']).replace('\n', '').replace('\r', '').strip()
-    collection.update_one({'_id': doc['_id']}, {'$set': {'Titre': titre}})
->>>>>>> Stashed changes
 
 # Récupérer tous les documents de la collection
 @app.route('/documents', methods=['GET'])
@@ -101,7 +96,7 @@ def get_document_by_limit(limit):
     documents = collection.find().limit(limit)
     output = []
     for document in documents:
-        output.append({'id': str(document['_id']), 'Titre': document['Titre'], 'Origine': document['Origine']})
+        output.append({'id': str(document['_id']), 'Titre': document['Titre'], 'Origine': document['Origine'], 'Total Chapter': document['Total Chapter'],'Date de sortie': document['Date de sortie'],'Type': document['Type'],'Image': document['Image'],'Source Statuts':document['Source Statuts']})
     return jsonify({'result': output})
 
 # Recuperer uniquement les documents dont le titre contient un mot
